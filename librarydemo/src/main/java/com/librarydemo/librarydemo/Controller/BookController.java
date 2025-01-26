bfcd2bb6104dafd3988f83760a7e765ed7fa36df
Rita <mustakimova_rita@mail.ru>
2025-01-26 14:43:57 +0500

    Обнова

--- librarydemo/src/main/java/com/librarydemo/librarydemo/Controller/BookController.java
+++ librarydemo/src/main/java/com/librarydemo/librarydemo/Controller/BookController.java
@@ -28,32 +28,32 @@ public class BookController {
         return "redirect:/books";
     }
 
-//    @GetMapping("/add")
-//    public String showAddForm(Model model) {
-//        model.addAttribute("book", new Book());
-//        return "add";
-//    }
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "add";
+    }
 
-//    @GetMapping("/edit/{id}")
-//    public String showEditForm(@PathVariable Long id, Model model) {
-//        Optional<Book> book = libraryService.findBookById(id);
-//        if (book.isPresent()) {
-//            model.addAttribute("book", book.get());
-//            return "edit";
-//        }
-//        return "redirect:/books";
-//    }
-//
-//    @PostMapping("/edit/{id}")
-//    public String editBook(@PathVariable Long id, @ModelAttribute Book book) {
-//        book.setIdBook(id);
-//        libraryService.saveBook(book);
-//        return "redirect:/books";
-//    }
-//
-//    @GetMapping("/delete/{id}")
-//    public String deleteBook(@PathVariable Long id) {
-//        libraryService.deleteBook(id);
-//        return "redirect:/books";
-//    }
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        Optional<Book> book = libraryService.findBookById(id);
+        if (book.isPresent()) {
+            model.addAttribute("book", book.get());
+            return "edit";
+        }
+        return "redirect:/books";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, @ModelAttribute Book book) {
+        book.setIdBook(id);
+        libraryService.saveBook(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        libraryService.deleteBook(id);
+        return "redirect:/books";
+    }
 }
